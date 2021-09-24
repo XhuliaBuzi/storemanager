069362fb25322f3242857182ecc680ace2813c02
dancarreon <daniel.carreon@clevertech.biz>
2021-09-24 11:37:32 +0200

    First commit

---
+++ src/test/java/com/storemanager/StoreManagerAppTests.java
@@ -0,0 +1,13 @@
+package com.storemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagerAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
