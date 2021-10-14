f76c6135b0a0fbdea5e5b300974c520b508cdb7b
Xhulia <xhuliabuzzi@gmail.com>
2021-10-14 12:14:17 +0200

    update

--- src/main/java/com/storemanager/repository/SaleRepository.java
+++ src/main/java/com/storemanager/repository/SaleRepository.java
@@ -3,7 +3,6 @@ package com.storemanager.repository;
 import com.storemanager.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 }
